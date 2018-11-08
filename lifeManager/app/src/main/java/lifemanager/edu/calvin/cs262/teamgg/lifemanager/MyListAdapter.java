6ccf7a6f38ed66aa2b020f049a380d23055b5986
Peter Haagsma <pjh26@maroon29.cs.calvin.edu>
2018-11-07 19:53:48 -0500

    Added new options button, options popup, and started edit implementation

--- lifeManager/app/src/main/java/lifemanager/edu/calvin/cs262/teamgg/lifemanager/MyListAdapter.java
+++ lifeManager/app/src/main/java/lifemanager/edu/calvin/cs262/teamgg/lifemanager/MyListAdapter.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -76,7 +78,7 @@ public class MyListAdapter extends ArrayAdapter<ScheduleCard> {
         TextView textViewCategory = view.findViewById(R.id.itemCategory);
         TextView textViewDescription = view.findViewById(R.id.itemDescription);
         TextView textViewTime = view.findViewById(R.id.itemTime);
-        Button openOptions = view.findViewById(R.id.options);
+        final TextView textViewOptions = view.findViewById(R.id.textViewOptions);
 
 
         //getting the scheduleCard of the specified position
@@ -90,22 +92,46 @@ public class MyListAdapter extends ArrayAdapter<ScheduleCard> {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Log.d("Success??", "SUCCESS!!");
-//                Log.d("Card Category", scheduleCard.getCardCategory());
-                //create the popup window
+                //create the popup window with detailed information
                 openDetails(scheduleCardList.indexOf(scheduleCard));
             }
         });
 
-        openOptions.setOnClickListener(new View.OnClickListener() {
+        textViewOptions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //we will call this method to remove the selected value from the list
-                //we are passing the position which is to be removed in the method
-//                Log.d("Success??", "SUCCESS!!");
-//                Log.d("Card Options", scheduleCard.getCardCategory());
-                openOptions(scheduleCardList.indexOf(scheduleCard));
+            //open the options menu
+                //openOptions(scheduleCardList.indexOf(scheduleCard));
+
+            PopupMenu popup = new PopupMenu(getContext(), textViewOptions);
+
+            popup.inflate(R.menu.card_menu);
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_view:
+                            openDetails(position);
+                            break;
+                        case R.id.action_edit:
+                            editCard(position);
+                            break;
+                        case R.id.action_share:
+
+                            break;
+                        case R.id.action_delete:
+                            deleteCard(position);
+                            break;
+                    }
+                    return false;
+                }
+            });
+
+            //displaying the popup
+            popup.show();
+
             }
+
         });
 
 
@@ -116,7 +142,7 @@ public class MyListAdapter extends ArrayAdapter<ScheduleCard> {
 
 
     //this method will remove the item from the list
-    private void openOptions(final int position) {
+    private void deleteCard(final int position) {
         //Creating an alert dialog to confirm the deletion
         AlertDialog.Builder builder = new AlertDialog.Builder(this.activity);
         builder.setTitle("Are you sure you want to delete this?");
@@ -152,9 +178,6 @@ public class MyListAdapter extends ArrayAdapter<ScheduleCard> {
 
     private void openDetails(final int position) {
         ScheduleCard scheduleCard = scheduleCardList.get(position);
-        //adding a click listener to the button to open details
-        //we need to get the view of the xml for our list item
-        //And for this we need a layoutinflater
 
         Dialog popupDialog = new Dialog(getContext());
         popupDialog.setContentView(R.layout.schedule_popup);
@@ -177,5 +200,20 @@ public class MyListAdapter extends ArrayAdapter<ScheduleCard> {
         wlp.y = 50;
         popupDialog.show();
 
+    }
+
+    private void editCard(final int position) {
+        ScheduleCard scheduleCard = scheduleCardList.get(position);
+
+        Dialog popupDialog = new Dialog(getContext());
+        popupDialog.setContentView(R.layout.fragment_new_event);
+
+        Window window = popupDialog.getWindow();
+
+        WindowManager.LayoutParams wlp = window.getAttributes();
+
+        wlp.gravity = Gravity.TOP;
+        wlp.y = 50;
+        popupDialog.show();
     }
 }
